7db974b541451632eb4c361dd194990f0105404e
AnjaliDheek06 <dheekanjali06@gmail.com>
2026-01-19 20:23:27 +0530

    Added Room and Course models with migrations and admin setup

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Room,Course                  #why?  To instantly add rooms,courses,edit/delete records
+admin .site.register(Room)                                           #Without this we need forms,views,templates just to insert data
+admin .site.register(Course)
--- exams/models.py
+++ exams/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):                                                            #why?  To create DB table Room
+    room_number = models.CharField(max_length=10)                                # sitsmart needs room
+    capacity = models.IntegerField()                                                              #This model will allow atomatic allocation based on capacity
+    building = models.CharField(max_length=50)                                          #Prevents over crowding and logical of rooms  (building/floor)
+    floor = models.IntegerField()                                                                    #Without this model automation is impossible
+
+
+
+class Course(models.Model):                                                          #why?  In seating exam system, course is required
+    code = models.CharField(max_length=10)                                             #Rules depend upon: same course student should not sit together , course-wise room distribution
+    name = models.CharField(max_length=100)                                           # will later be connected with student, exam ,seating algorithm                               
+    credits = models. IntegerField()                                                             
+
+
+
